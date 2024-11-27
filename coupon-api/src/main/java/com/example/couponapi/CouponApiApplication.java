84fe224319ae5bc49fab4039d4fe2d0dedc77508
fullm <fullmoon0207@naver.com>
2024-11-28 01:03:55 +0900

    build: 프로젝트 초기 세팅

--- coupon-api/src/main/java/com/example/couponapi/CouponApiApplication.java
+++ coupon-api/src/main/java/com/example/couponapi/CouponApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CouponApiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application-core, application-api");
 		SpringApplication.run(CouponApiApplication.class, args);
 	}
 
