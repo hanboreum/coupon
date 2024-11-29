57a4614ea40a878662840d758c33a617573af396
fullm <fullmoon0207@naver.com>
2024-11-29 20:20:36 +0900

    feat: 쿠폰 발급 관련 로직

---
+++ coupon-core/src/main/java/com/example/couponcore/service/CouponIssueService.java
@@ -0,0 +1,56 @@
+package com.example.couponcore.service;
+
+import static com.example.couponcore.exception.ErrorCode.COUPON_NOT_EXIST;
+
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.exception.ErrorCode;
+import com.example.couponcore.model.Coupon;
+import com.example.couponcore.model.CouponIssue;
+import com.example.couponcore.repository.mysql.CouponIssueJpaRepository;
+import com.example.couponcore.repository.mysql.CouponIssueRepository;
+import com.example.couponcore.repository.mysql.CouponJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CouponIssueService {
+
+    private final CouponJpaRepository couponJpaRepository;
+    private final CouponIssueJpaRepository couponIssueJpaRepository;
+    private final CouponIssueRepository couponIssueRepository;
+
+    @Transactional
+    public void issue(long couponId, long userId) {
+        Coupon coupon = findCoupon(couponId);
+        coupon.issue();
+        saveCouponIssue(couponId, userId);
+    }
+
+    @Transactional(readOnly = true)
+    public Coupon findCoupon(long couponId) {
+        return couponJpaRepository.findById(couponId).orElseThrow(() -> { //TODO exception -> repo
+            throw new CouponIssueException(COUPON_NOT_EXIST,
+                    "쿠폰 정책이 존재 하지 않는다. %s".formatted(couponId));
+        });
+    }
+
+    @Transactional
+    public CouponIssue saveCouponIssue(long couponId, long userId) {
+        checkAlreadyIssuance(couponId, userId);
+        CouponIssue couponIssue = CouponIssue.builder()
+                .couponId(couponId)
+                .userId(userId)
+                .build();
+        return couponIssueJpaRepository.save(couponIssue);
+    }
+
+    private void checkAlreadyIssuance(long couponId, long userId) {
+        CouponIssue issue = couponIssueRepository.findFirstCouponIssue(couponId, userId);
+        if (issue != null) {
+            throw new CouponIssueException(ErrorCode.DUPLICATED_COUPON_ISSUE,
+                    "이미 발급된 쿠폰 입니다. user_id: %d, coupon_id: %d".formatted(userId, couponId));
+        }
+    }
+}
