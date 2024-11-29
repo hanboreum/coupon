2b0a1581f543018523b2e0dcf582af1bf798ddf6
fullm <fullmoon0207@naver.com>
2024-11-29 20:17:36 +0900

    fix: Spring boot Could not autowire

--- coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
@@ -1,12 +1,14 @@
 package com.example.couponcore;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class CouponCoreConfiguration {
 
 }
