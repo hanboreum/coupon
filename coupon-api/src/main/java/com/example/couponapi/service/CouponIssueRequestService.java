8e2d6b286060447c08a4808df5f470a0ca349721
fullm <fullmoon0207@naver.com>
2024-11-30 16:42:35 +0900

    feat: redis 분산락 적용

--- coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.example.couponapi.service;
 
 import com.example.couponapi.controller.dto.CouponIssueRequestDto;
+import com.example.couponcore.component.DistributeLockExecutor;
 import com.example.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,16 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        synchronized (this) {
+        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () -> {
+            /**
+             * executor 에 runnable 을 넘겨 execute 안에서 실행 할 수 있도록.
+             */
             couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        }
+
+        });
         log.info("쿠폰 발급 완료. couponId: {}, userId: {}", requestDto.couponId(), requestDto.userId());
     }
 }
