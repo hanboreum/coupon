ffb7a8a9a7685ace6fa4053d01d7a61d96a7bc61
fullm <fullmoon0207@naver.com>
2024-12-02 21:15:23 +0900

    test: redis sorted set 을 사용한 쿠폰 발급 요청 성능 테스트

---
+++ load-test/locustfile-async-issueV1.py
@@ -0,0 +1,16 @@
+import random
+from locust import task, FastHttpUser
+# docker compose up -d --scale worker=3
+
+class CouponTestV1(FastHttpUser):
+  connection_timeout = 10.0  #
+  network_timeout = 10.0  # 10초만 기다림
+
+  @task
+  def issue(self):
+    payload = {
+      "userId": random.randint(1, 10000000),
+      "couponId": 1
+    }
+    with self.rest("POST", "/v1/issue-async", json=payload):
+      pass
