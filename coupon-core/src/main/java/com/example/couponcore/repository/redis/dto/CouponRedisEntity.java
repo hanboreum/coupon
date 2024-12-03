bc91464834341fbc02470cf17792b7567019028e
fullm <fullmoon0207@naver.com>
2024-12-03 17:51:50 +0900

    feat: cache entity, service

---
+++ coupon-core/src/main/java/com/example/couponcore/repository/redis/dto/CouponRedisEntity.java
@@ -0,0 +1,53 @@
+package com.example.couponcore.repository.redis.dto;
+
+import static com.example.couponcore.exception.ErrorCode.INVALID_COUPON_ISSUE_DATE;
+
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.exception.ErrorCode;
+import com.example.couponcore.model.Coupon;
+import com.example.couponcore.model.CouponType;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+
+//이 엔티티를 통해 유효성 검사를 한다. -> 기존 db조회 하던 것들을 캐시 조회로 변환
+public record CouponRedisEntity(
+        Long id,
+        CouponType couponType,
+        Integer totalQuantity,
+
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueStart,
+
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+
+        LocalDateTime dateIssueEnd
+) {
+
+    public CouponRedisEntity(Coupon coupon) {
+        this(
+                coupon.getId(),
+                coupon.getCouponType(),
+                coupon.getTotalQuantity(),
+                coupon.getDateIssueStart(),
+                coupon.getDateIssueEnd()
+        );
+    }
+
+    private boolean availableIssueDate() {
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+
+    public void checkIssuableCoupon() {
+        if (!availableIssueDate()) {
+            throw new CouponIssueException(INVALID_COUPON_ISSUE_DATE,
+                    " 발급 기간이 유효 하지 않음, couponId: %s, issueStart:%s, issueEnd:%s".formatted(id,
+                            dateIssueStart, dateIssueEnd));
+        }
+    }
+}
