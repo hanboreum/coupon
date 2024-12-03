1a1c7c0d0624d95c2b229206570055dce8c3c3de
fullm <fullmoon0207@naver.com>
2024-12-03 16:59:44 +0900

    feat: 에러 코드 추가

--- coupon-core/src/main/java/com/example/couponcore/exception/ErrorCode.java
+++ coupon-core/src/main/java/com/example/couponcore/exception/ErrorCode.java
@@ -4,7 +4,8 @@ public enum ErrorCode {
     INVALID_COUPON_ISSUE_QUANTITY("발급 가능 수량 초과"),
     INVALID_COUPON_ISSUE_DATE("유효하지 않은 발급 기간"),
     COUPON_NOT_EXIST("존재하지 않는 구폰"),
-    DUPLICATED_COUPON_ISSUE("이미 발급된 쿠폰");
+    DUPLICATED_COUPON_ISSUE("이미 발급된 쿠폰"),
+    FAIL_COUPON_REQUEST("쿠폰 발급 요청에 실패");
 
     public final String message;
 
